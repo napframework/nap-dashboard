d8a0612c155baa20174edbb479d1ffb0dfd85a42
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-21 16:31:15 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppMenu from './components/AppMenu.vue';
+</script>
+
+<template>
+  <AppMenu />
+  <router-view id="page"></router-view>
+</template>
+
+<style>
+body {
+  margin: 0;
+  color: #ffffff;
+  background: #000000;
+}
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#page {
+  padding: 24px;
+}
+</style>
