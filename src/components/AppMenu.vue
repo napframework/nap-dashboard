2c2d3a3305aba2a34b6883de08e6d967d69f369f
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-21 16:30:35 +0100

    Create AppMenu.vue

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app-menu">
+
+    <router-link to="/">
+      <img alt="NAP logo" src="../assets/logo.png" />
+    </router-link>
+
+    <router-link to="/portal-component">
+      Portal Component
+    </router-link>
+
+  </div>
+</template>
+
+<style scoped>
+#app-menu {
+  display: flex;
+  align-items: flex-end;
+  border-bottom: solid 1px #ffffff;
+}
+a {
+  color: white;
+  padding: 12px;
+  white-space: nowrap;
+  text-decoration: none;
+}
+a:hover,
+a.router-link-active {
+  color: #c86969;
+}
+img {
+  height: 48px;
+}
+</style>
