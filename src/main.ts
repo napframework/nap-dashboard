5ea0b06e618b092fb705abbd3e2266955d36f88a
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-22 13:31:44 +0100

    Create NAPWebSocket connection

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { NAPWebSocket } from 'nap-portal';
 import { createApp } from 'vue';
 import {
   createRouter,
@@ -8,12 +9,35 @@ import App from './App.vue';
 import HomePage from './pages/HomePage.vue';
 import PortalPage from './pages/PortalPage.vue';
 
+// Create NAPWebSocket connection
+const napWebSocket = new NAPWebSocket({
+  host: 'localhost',
+  port: 2000,
+  user: 'napuser',
+  pass: 'letmein!',
+  secure: false,
+});
+
+// Open NAPWebSocket connection
+napWebSocket.open()
+  .then(() => console.log('NAPWebSocket opened successfully'))
+  .catch(error => console.error(`NAPWebSocket failed to open: ${error.message}`));
+
+// Create Vue Router
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomePage },
-    { path: '/:portal', component: PortalPage },
+    {
+      path: '/',
+      component: HomePage,
+    },
+    {
+      path: '/:portal',
+      component: PortalPage,
+      props: { napWebSocket }
+    },
   ],
 });
 
+// Create Vue App
 createApp(App).use(router).mount('#app');
