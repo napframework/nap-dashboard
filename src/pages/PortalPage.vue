1b6ee3a0b8bcccc90a1f5034a6140734cbfd2b7d
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-22 14:15:52 +0100

    Update PortalPage.vue

--- src/pages/PortalPage.vue
+++ src/pages/PortalPage.vue
@@ -1,5 +1,49 @@
+<script setup lang="ts">
+import {
+  NAPPortal,
+  NAPWebSocket,
+} from 'nap-portal';
+import {
+  ref,
+  onMounted,
+  onBeforeUnmount,
+} from 'vue';
+
+// Define props
+const props = defineProps({
+  napWebSocket: NAPWebSocket,
+});
+
+// Expose portal element
+const portal = ref(null);
+defineExpose({ portal });
+
+// NAPPortal variable
+let napPortal: NAPPortal | null = null;
+
+// Create portal on mount
+onMounted(() => {
+  if (napPortal === null) {
+    napPortal = new NAPPortal({
+      el: portal.value!,
+      portalId: 'PortalComponent',
+      napWebSocket: props.napWebSocket!,
+    });
+  }
+});
+
+// Destroy portal on unmount
+onBeforeUnmount(() => {
+  if (napPortal !== null) {
+    napPortal.destroy();
+    napPortal = null;
+  }
+});
+</script>
+
 <template>
   <div>
     Welcome to your NAP portal!
+    <div ref="portal"></div>
   </div>
 </template>
