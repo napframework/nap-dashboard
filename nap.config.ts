14f20ce99e44629262221338e7d7023ba9a22493
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-29 00:09:25 +0100

    Create nap.config.ts

---
+++ nap.config.ts
@@ -0,0 +1,14 @@
+export default {
+  portals: [
+    {
+      id: 'PortalComponent',
+      name: 'Portal Component',
+      path: 'portal-component',
+    },
+    {
+      id: 'PortalComponent2',
+      name: 'Portal Component 2',
+      path: 'portal-component-2',
+    },
+  ],
+};
